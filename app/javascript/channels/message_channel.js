024775fadb574f1191f637c5b0b7bd73f0b86a53
YutoYasunaga <yuto.yasunaga@gmail.com>
2024-02-24 15:54:26 +0900

    Refactor

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -3,7 +3,9 @@ import consumer from "./consumer"
 consumer.subscriptions.create("MessageChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
-    this.scrollToBottom()
+    document.addEventListener('turbo:load', () => {
+      this.scrollToBottom()
+    })
   },
 
   disconnected() {
@@ -15,7 +17,7 @@ consumer.subscriptions.create("MessageChannel", {
   },
 
   scrollToBottom() {
-    var messagesBox = document.getElementById('messages')
+    const messagesBox = document.getElementById('messages')
     setTimeout(() => {
       messagesBox.scrollTop = messagesBox.scrollHeight;
     }, 100)
--- app/javascript/controllers/messages_controller.js
+++ app/javascript/controllers/messages_controller.js
@@ -1,15 +1,44 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
+  initialize() {
+    this.isTyping = false
+    this.typingTimer = null
+    this.doneTypingInterval = 3000
   }
 
-  disconnect() {
+  connect() {
+    // Directly attach event listener to the body input by ID
+    const bodyField = document.getElementById('message_body');
+    bodyField.addEventListener('input', () => this.userTyping());
+
+    // Listen for beforeunload if needed
+    window.addEventListener('beforeunload', () => this.doneTyping());
   }
 
   resetForm(event) {
     if (event.detail.success) {
-      this.element.querySelector('textarea').value = ''
+      const bodyField = document.getElementById('message_body');
+      bodyField.value = '';
+    }
+  }
+
+  userTyping() {
+    if (!this.isTyping) {
+      const usernameField = document.getElementById('message_username');
+      const currentUserId = document.body.dataset.userId;
+      App.typingChannel.perform('typing', { active: true, username: usernameField.value, user_id: currentUserId });
+      this.isTyping = true;
+    }
+
+    clearTimeout(this.typingTimer);
+    this.typingTimer = setTimeout(() => this.doneTyping(), this.doneTypingInterval);
+  }
+
+  doneTyping() {
+    if (this.isTyping) {
+      App.typingChannel.perform('typing', { active: false });
+      this.isTyping = false;
     }
   }
 }
